1537262e15264514a1f94f07a6718a2ebb92c6ca
Sadra <sadra.19@gmail.com>
2023-11-29 10:58:48 +0330

    Pref: Blocked phone number created at

--- resources/views/admin/blockedPhoneNumbers.blade.php
+++ resources/views/admin/blockedPhoneNumbers.blade.php
@@ -8,20 +8,14 @@
         </h5>
         <div class="card-body">
 
-            <button type="button" class="btn btn-primary mb-3"
-                    data-bs-toggle="modal"
-                    data-bs-target="#blockPhoneNumberForm">
+            <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#blockPhoneNumberForm">
                 وارد کردن شماره تلفن به لیست ممنوعه
             </button>
-            <div id="blockPhoneNumberForm" class="modal fade"
-                 role="dialog">
+            <div id="blockPhoneNumberForm" class="modal fade" role="dialog">
                 <div class="modal-dialog">
 
                     <!-- Modal content-->
-                    <form
-                        action="{{ url('admin/blockPhoneNumber') }}"
-                        method="post"
-                        class="modal-content">
+                    <form action="{{ url('admin/blockPhoneNumber') }}" method="post" class="modal-content">
                         @csrf
                         <div class="modal-header">
                             <h4 class="modal-title">وارد کردن شماره تلفن به لیست ممنوعه</h4>
@@ -30,15 +24,14 @@
 
                             <div class="form-group col-lg-12">
                                 <input class="m-1 form-control" name="phoneNumber" type="tel"
-                                       placeholder="شماره مورد نظر را وارد نمایید">
+                                    placeholder="شماره مورد نظر را وارد نمایید">
                             </div>
                             <div class="form-group col-lg-12">
                                 <input class="m-1 form-control" name="name" type="text"
-                                       placeholder="نام و نام خانوادگی صاحب شماره تلفن">
+                                    placeholder="نام و نام خانوادگی صاحب شماره تلفن">
                             </div>
                             <div class="form-group col-lg-12">
-                <textarea class=" m-1 form-control" name="description"
-                          placeholder="توضیحات : علت ممنوع بودن"></textarea>
+                                <textarea class=" m-1 form-control" name="description" placeholder="توضیحات : علت ممنوع بودن"></textarea>
                             </div>
                         </div>
                         <div class="modal-footer text-left">
@@ -53,35 +46,40 @@
 
             <table class="table">
                 <thead>
-                <tr>
-                    <th>#</th>
-                    <th>شماره</th>
-                    <th>نام و نام خانوادگی</th>
-                    <th>توضیحات</th>
-                    <th>حذف از لیست</th>
-                </tr>
+                    <tr>
+                        <th>#</th>
+                        <th>شماره</th>
+                        <th>نام و نام خانوادگی</th>
+                        <th>توضیحات</th>
+                        <th>حذف از لیست</th>
+                        <th>تاریخ</th>
+                    </tr>
                 </thead>
                 <tbody class="small text-right">
-                <?php $i = 1;?>
-                @foreach($blockedPhoneNumbers as $blockedPhoneNumber)
-                    <tr>
-                        <td>{{ $i++ }}</td>
-                        <td>
-                            {{ $blockedPhoneNumber->phoneNumber }}
-                        </td>
-                        <td>
-                            {{ $blockedPhoneNumber->name }}
-                        </td>
-                        <td>
-                            {{ $blockedPhoneNumber->description }}
-                        </td>
-                        <td>
-                            <a class="btn btn-sm btn-danger"
-                               href="{{ url('admin/unblockPhoneNumber') }}/{{ $blockedPhoneNumber->phoneNumber }}">حذف
-                                از لیست</a>
-                        </td>
-                    </tr>
-                @endforeach
+                    <?php $i = 1; ?>
+                    @foreach ($blockedPhoneNumbers as $blockedPhoneNumber)
+                        <tr>
+                            <td>{{ $i++ }}</td>
+                            <td>
+                                {{ $blockedPhoneNumber->phoneNumber }}
+                            </td>
+                            <td>
+                                {{ $blockedPhoneNumber->name }}
+                            </td>
+                            <td>
+                                {{ $blockedPhoneNumber->description }}
+                            </td>
+                            <td>
+                                <a class="btn btn-sm btn-danger"
+                                    href="{{ url('admin/unblockPhoneNumber') }}/{{ $blockedPhoneNumber->phoneNumber }}">حذف
+                                    از لیست</a>
+                            </td>
+                            @php
+                                $pieces = explode(" ", $blockedPhoneNumber->created_at);
+                            @endphp
+                        <td dir="ltr">{{ gregorianDateToPersian($blockedPhoneNumber->created_at, '-', true) . ' ' . $pieces[1] }}</td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
 
