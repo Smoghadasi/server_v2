fc2c858daad337aae375de75c4b822b41c83a49d
Sadra <sadra.19@gmail.com>
2023-12-30 09:20:32 +0330

    Add: param to loadbackup

--- resources/views/admin/customerLoads.blade.php
+++ resources/views/admin/customerLoads.blade.php
@@ -13,10 +13,11 @@
                 <tr>
                     <th>#</th>
                     <th>عنوان بار</th>
-                    <th>عرض</th>
-                    <th>طول</th>
-                    <th>ارتفاء</th>
-                    <th>وزن</th>
+                    <th>شماره تلفن</th>
+                    <th>مبدا</th>
+                    <th>مقصد</th>
+                    <th>قیمت</th>
+                    <th>ناوگان</th>
                     <th>تاریخ</th>
                     <th>نمایش</th>
                 </tr>
@@ -27,10 +28,18 @@
                     <tr>
                         <td>{{ $key + 1 }}</td>
                         <td>{{ $load->title }}</td>
-                        <td>{{ $load->width }}</td>
-                        <td>{{ $load->lenght }}</td>
-                        <td>{{ $load->height }}</td>
-                        <td>{{ $load->weight }}</td>
+                        <td>{{ $load->mobileNumberForCoordination }}</td>
+                        <td>{{ $load->fromCity }}</td>
+                        <td>{{ $load->toCity }}</td>
+                        <td>{{ $load->priceBased }}</td>
+                        <td>
+                            @php
+                                $fleets = json_decode($load->fleets, true);
+                            @endphp
+                            @foreach ($fleets as $fleet)
+                                <span class="alert alert-primary p-1 m-1 small" style="line-height: 2rem">{{ $fleet['title'] }}</span>
+                            @endforeach
+                        </td>
                         <td>{{ $load->loadingDate }}</td>
                         <td><a href="{{ url('admin/loadInfo') }}/{{ $load->id }}">نمایش جزئیات</a></td>
                     </tr>
