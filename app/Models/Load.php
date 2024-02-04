7e4d1d421ae09f62c048584d961f22b179e32aa1
Sadra Moghadasi <sadra.19@gmail.com>
2024-02-04 13:30:20 +0330

    Add: Softdelete in loads

--- app/Models/Load.php
+++ app/Models/Load.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Load extends Model
 {
+
+    use SoftDeletes;
     private static $transportationCompany_id;
     protected $appends = ['numOfRequestedDrivers', 'numOfSelectedDrivers', 'originCity', 'destinationCity'];
 
