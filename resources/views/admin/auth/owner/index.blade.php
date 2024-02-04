ca92d51ba096819bca907236650ab47a8c96c151
Sadra Moghadasi <sadra.19@gmail.com>
2024-02-04 09:16:32 +0330

    Add: date to owner index

--- resources/views/admin/auth/owner/index.blade.php
+++ resources/views/admin/auth/owner/index.blade.php
@@ -13,41 +13,48 @@
                 <a href="{{ route('owner.reject') }}" class="alert p-1 alert-danger">تایید نشده : {{ $ownerRejectCounts }}</a>
             </div>
             <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>نام و نام خانوادگی</th>
-                        <th>کد ملی</th>
-                        <th>شماره موبایل</th>
-                        <th>عملیات</th>
-                    </tr>
-                </thead>
-                <tbody class="small">
-                    <?php $i = 0; ?>
-                    @forelse($owners as $owner)
+                <table class="table">
+                    <thead>
                         <tr>
-                            <td>{{ ($owners->currentPage() - 1) * $owners->perPage() + ++$i }}</td>
-                            <td>{{ $owner->name }} {{ $owner->lastName }}
-                                {{ $owner->isOwner == 1 ? '(صاحب بار)' : '(باربری)' }}</td>
-                            <td>{{ $owner->nationalCode }}</td>
-                            <td>{{ $owner->mobileNumber }}</td>
-                            <td>
-                                <a class="btn btn-sm btn-primary" href="{{ route('ownerAuth.edit', $owner) }}">بررسی
-                                    اطلاعات</a>
-                            </td>
+                            <th>#</th>
+                            <th>نام و نام خانوادگی</th>
+                            <th>کد ملی</th>
+                            <th>شماره موبایل</th>
+                            <th>تاریخ ثبت نام</th>
+                            <th>عملیات</th>
                         </tr>
-                    @empty
-                        <tr class="text-center">
-                            <td colspan="10">فیلد مورد خالی است</td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
-            <div class="mt-2">
-                {{ $owners }}
+                    </thead>
+                    <tbody class="small">
+                        <?php $i = 0; ?>
+                        @forelse($owners as $owner)
+                            <tr>
+                                <td>{{ ($owners->currentPage() - 1) * $owners->perPage() + ++$i }}</td>
+                                <td>{{ $owner->name }} {{ $owner->lastName }}
+                                    {{ $owner->isOwner == 1 ? '(صاحب بار)' : '(باربری)' }}</td>
+                                <td>{{ $owner->nationalCode }}</td>
+                                <td>{{ $owner->mobileNumber }}</td>
+                                @php
+                                    $pieces = explode(' ', $owner->created_at);
+                                @endphp
+                                <td dir="ltr">
+                                    {{ gregorianDateToPersian($owner->created_at, '-', true) . ' ' . $pieces[1] }}
+                                </td>
+                                <td>
+                                    <a class="btn btn-sm btn-primary" href="{{ route('ownerAuth.edit', $owner) }}">بررسی
+                                        اطلاعات</a>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr class="text-center">
+                                <td colspan="10">فیلد مورد خالی است</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+                <div class="mt-2">
+                    {{ $owners }}
+                </div>
             </div>
-        </div>
         </div>
 
     </div>
