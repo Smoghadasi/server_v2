ca3f95b338e4f9ee5adeff23bd458e6f22965fbc
Sadra Moghadasi <sadra.19@gmail.com>
2024-02-17 13:54:44 +0330

    Add: search in report by fleet

--- app/Http/Controllers/ReportingController.php
+++ app/Http/Controllers/ReportingController.php
@@ -1127,7 +1127,7 @@ class ReportingController extends Controller
             $cargoReports = CargoReportByFleet::with('fleet')
                 ->where('fleet_id', $request->fleet_id)
                 ->orderByDesc('date')
-                ->paginate(25);
+                ->get();
             $fleets = Fleet::where('parent_id', '>', 0)->orderBy('parent_id', 'asc')->get();
             return view('admin.reporting.cargoFleetsReport', compact('cargoReports', 'fleets'));
         } catch (\Exception $exception) {
