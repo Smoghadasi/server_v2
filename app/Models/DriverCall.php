75a21beb430473139a2821934ff06a99429b1c88
Sadra Moghadasi <sadra.19@gmail.com>
2024-04-21 12:12:55 +0330

    Add: property

--- app/Models/DriverCall.php
+++ app/Models/DriverCall.php
@@ -9,7 +9,8 @@ class DriverCall extends Model
 {
     protected $appends = [
         'isAccepted',
-        'isAnyOneSelectedDriver'
+        'isAnyOneSelectedDriver',
+        'avarageRateDriver'
     ];
     /**
      * Get the user that owns the DriverCall
@@ -46,5 +47,10 @@ class DriverCall extends Model
         } else {
             return false;
         }
+    }
+
+    public function getAvarageRateDriverAttribute()
+    {
+        return Score::where('type', 'Owner')->where('driver_id', $this->driver_id)->avg('value');
     }
 }
