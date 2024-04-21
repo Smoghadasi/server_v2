81e2821e213e73cba06f29357ad8b9b1b6bed62d
Sadra Moghadasi <sadra.19@gmail.com>
2024-04-21 10:27:02 +0330

    Add: propery

--- app/Models/DriverLoad.php
+++ app/Models/DriverLoad.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class DriverLoad extends Model
 {
+    protected $appends = [
+        'numOfRateDriver',
+    ];
 
+    public function getNumOfRateDriverAttribute()
+    {
+        $driverLoad = DriverLoad::where('type', 'Driver')
+            ->where('owner_id', $this->owner_id)
+            ->where('driver_id', $this->driver_id)
+            ->first();
+        return $driverLoad->value;
+    }
 }
