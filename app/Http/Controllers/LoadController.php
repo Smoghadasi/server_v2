cc3ca5aa867cdc91d4fa4fb5dde3559f8166c4eb
Sadra Moghadasi <sadra.19@gmail.com>
2024-05-16 08:53:36 +0330

    Add: send message in province drivers

--- app/Http/Controllers/LoadController.php
+++ app/Http/Controllers/LoadController.php
@@ -906,15 +906,17 @@ class LoadController extends Controller
                     $cityFrom = ProvinceCity::where('id', $load->origin_city_id)->first();
                     $cityTo = ProvinceCity::where('id', $load->destination_city_id)->first();
 
-                    // ارسال پیام برای شهرستان
-                    $drivers = Driver::where('city_id', $cityFrom->id)
-                        ->where('fleet_id', $fleet->fleet_id)
-                        ->get();
-
-                    // // ارسال پیام برای استان
-                    // $drivers = Driver::where('province_id', $cityFrom->parent_id)
+                    // // ارسال پیام برای شهرستان
+                    // $drivers = Driver::where('city_id', $cityFrom->id)
                     //     ->where('fleet_id', $fleet->fleet_id)
                     //     ->get();
+
+                    // ارسال پیام برای استان
+                    $drivers = Driver::where('province_id', $cityFrom->parent_id)
+                        ->where('fleet_id', $fleet->fleet_id)
+                        ->where('sendMessage', 0)
+                        ->take(40)
+                        ->get();
 
                     if (isset($drivers)) {
                         foreach ($drivers as $driver) {
@@ -925,6 +927,28 @@ class LoadController extends Controller
                                 $cityFrom->name,
                                 $cityTo->name
                             );
+                        }
+                    } else {
+                        Driver::where('province_id', $cityFrom->parent_id)
+                            ->where('fleet_id', $fleet->fleet_id)
+                            ->where('sendMessage', 1)
+                            ->update(['sendMessage' => 0]);
+
+                        $drivers = Driver::where('province_id', $cityFrom->parent_id)
+                            ->where('fleet_id', $fleet->fleet_id)
+                            ->where('sendMessage', 0)
+                            ->take(40)
+                            ->get();
+                        if (isset($drivers)) {
+                            foreach ($drivers as $driver) {
+                                $sms = new Driver();
+                                $sms->subscriptionLoadSmsIr(
+                                    $driver->mobileNumber,
+                                    $driver->name,
+                                    $cityFrom->name,
+                                    $cityTo->name
+                                );
+                            }
                         }
                     }
                 } catch (\Exception $exception) {
