51365ac840fecad08785f440137a7eb4fd2e3f75
Sadra Moghadasi <sadra.19@gmail.com>
2024-05-19 10:39:36 +0330

    Add: setting in dashboard

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -13,15 +13,17 @@
                         @csrf
                         @method('put')
                         <div class="row">
-                            <div class="mb-3 col-md-6">
-                                <label for="tel" class="form-label">تلفن</label>
-                                <select id="tel" name="tel" class="select2 form-select">
-                                    <option value="02128420609" @if($setting->tel == '02128420609') selected @endif>02128420609</option>
-                                    <option value="02191097220" @if($setting->tel == '02191097220') selected @endif>02191097220</option>
-                                    <option value="09184696188" @if($setting->tel == '09184696188') selected @endif>09184696188</option>
-                                    <option value="08338390328" @if($setting->tel == '08338390328') selected @endif>08338390328</option>
-                                </select>
-                            </div>
+                            @if (Auth::id() == 40)
+                                <div class="mb-3 col-md-6">
+                                    <label for="tel" class="form-label">تلفن</label>
+                                    <select id="tel" name="tel" class="select2 form-select">
+                                        <option value="02128420609" @if($setting->tel == '02128420609') selected @endif>02128420609</option>
+                                        <option value="02191097220" @if($setting->tel == '02191097220') selected @endif>02191097220</option>
+                                        <option value="09184696188" @if($setting->tel == '09184696188') selected @endif>09184696188</option>
+                                        <option value="08338390328" @if($setting->tel == '08338390328') selected @endif>08338390328</option>
+                                    </select>
+                                </div>
+                            @endif
                             <div class="mb-3 col-md-6">
                                 <label for="sms_panel" class="form-label">پنل اس ام اس</label>
                                 <select id="sms_panel" name="sms_panel" class="select2 form-select">
