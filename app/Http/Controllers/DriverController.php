5728991bf379d08c30c4f879dbb2334acbb7ef59
Sadra Moghadasi <sadra.19@gmail.com>
2024-05-21 09:34:04 +0330

    Update: creditDriver

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -999,7 +999,7 @@ class DriverController extends Controller
         $setting = Setting::first();
 
         if ($request->month == 0) {
-            if (Auth::user()->role != 'admin' || Auth::id() != 26) {
+            if (Auth::user()->role != 'admin' || Auth::id() == 26) {
                 if ($this->updateActivationDateAndFreeCallsAndFreeAcceptLoads($driver, $request->month, $request->freeCalls, $driver->freeAcceptLoads)) {
                     $persian_date = gregorianDateToPersian(date('Y/m/d', time()), '/');
                     $oneMonth = gregorianDateToPersian(date('Y/m/d', strtotime('+30 day', time())), '/');
