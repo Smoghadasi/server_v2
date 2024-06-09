a9585bfb39106144fd5d1f47efc4524d0ffd0333
Sadra Moghadasi <sadra.19@gmail.com>
2024-06-09 14:04:14 +0330

    Add: notification in new load

--- app/Http/Controllers/LoadController.php
+++ app/Http/Controllers/LoadController.php
@@ -3157,6 +3157,47 @@ class LoadController extends Controller
 
     private function sendNotification($FCM_token, $title, $body, $API_ACCESS_KEY)
     {
+        $serviceAccountPath = './zarin-tarabar-firebase-adminsdk-9x6c3-699279e25d.json';
+        $serviceAccount = json_decode(file_get_contents($serviceAccountPath), true);
+
+        $clientEmail = $serviceAccount['client_email'];
+        $privateKey = $serviceAccount['private_key'];
+
+        $header = json_encode(['alg' => 'RS256', 'typ' => 'JWT']);
+        $now = time();
+        $expiration = $now + 3600;
+        $payload = json_encode([
+            'iss' => $clientEmail,
+            'scope' => 'https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/firebase.messaging',
+            'aud' => 'https://oauth2.googleapis.com/token',
+            'exp' => $expiration,
+            'iat' => $now
+        ]);
+
+        // Encode to base64
+        $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+        $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+        // Create the signature
+        $signatureInput = $base64UrlHeader . "." . $base64UrlPayload;
+        openssl_sign($signatureInput, $signature, $privateKey, 'sha256');
+        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+        // Create the JWT
+        $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+        // Exchange JWT for an access token
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://oauth2.googleapis.com/token');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+            'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+            'assertion' => $jwt
+        ]));
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $responseData = json_decode($response, true);
+        $accessToken = $responseData['access_token'];
+
         $url = 'https://fcm.googleapis.com/v1/projects/zarin-tarabar/messages:send';
         $notification = [
             "message" => [
@@ -3173,13 +3214,9 @@ class LoadController extends Controller
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Authorization: Bearer ' . $API_ACCESS_KEY,
+            'Authorization: Bearer ' . $accessToken,
             'Content-Type: application/json'
         ]);
-        $result = curl_exec($ch);
-        if ($result === FALSE) {
-            echo curl_error($ch);
-        }
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($notification));
         curl_exec($ch);
         curl_close($ch);
