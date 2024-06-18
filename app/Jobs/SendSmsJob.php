d5889a358955f45390ffa1859e2c207ded454634
Sadra Moghadasi <sadra.19@gmail.com>
2024-06-18 14:05:57 +0330

    Add: send sms after response

--- app/Jobs/SendSmsJob.php
+++ app/Jobs/SendSmsJob.php
@@ -41,7 +41,7 @@ class SendSmsJob implements ShouldQueue
         $cityTo = ProvinceCity::where('id', $load->destination_city_id)->first();
 
         $drivers = Driver::where('province_id', $cityFrom->parent_id)
-            ->where('id', 45172)
+            // ->where('id', 45172)
             ->where('fleet_id', $fleet->fleet_id)
             ->where('sendMessage', 0)
             ->take(15)
@@ -62,11 +62,13 @@ class SendSmsJob implements ShouldQueue
         } else {
             Driver::where('province_id', $cityFrom->parent_id)
                 ->where('fleet_id', $fleet->fleet_id)
+                // ->where('id', 45172)
                 ->where('sendMessage', 1)
                 ->update(['sendMessage' => 0]);
 
             $drivers = Driver::where('province_id', $cityFrom->parent_id)
                 ->where('fleet_id', $fleet->fleet_id)
+                // ->where('id', 45172)
                 ->where('sendMessage', 0)
                 ->take(40)
                 ->get();
