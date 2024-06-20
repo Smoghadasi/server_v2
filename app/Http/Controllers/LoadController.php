1f770ebfa68b3de58992d08eb22d642526adbdb0
Sadra Moghadasi <sadra.19@gmail.com>
2024-06-20 12:50:08 +0330

    Update: send notif

--- app/Http/Controllers/LoadController.php
+++ app/Http/Controllers/LoadController.php
@@ -1014,8 +1014,20 @@ class LoadController extends Controller
         }
 
         try {
-            event(new PostCargoNotificationEvent($load));
+            $fleet = FleetLoad::where('load_id', $load->id)->first();
+            $cityFrom = ProvinceCity::where('id', $load->origin_city_id)->first();
+            $cityTo = ProvinceCity::where('id', $load->destination_city_id)->first();
 
+            $driverFCM_tokens = Driver::whereNotNull('FCM_token')
+                ->where('province_id', $cityFrom->parent_id)
+                ->where('fleet_id', $fleet->fleet_id)
+                ->where('version', '>', 58)
+                ->pluck('FCM_token');
+            $title = 'ایران ترابر رانندگان';
+            $body = ' بار ' . $fleet->fleet->title . ':' . ' از ' . $cityFrom->name . ' به ' . $cityTo->name;
+            foreach ($driverFCM_tokens as $driverFCM_token) {
+                $this->sendNotification($driverFCM_token, $title, $body, API_ACCESS_KEY_OWNER);
+            }
         } catch (\Exception $exception) {
             Log::emergency("----------------------send notification load by driver-----------------------");
             Log::emergency($exception);
