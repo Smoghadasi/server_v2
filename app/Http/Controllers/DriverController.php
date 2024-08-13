acd3eda18d27e961e4b43c8f9ac76b5dd091c431
Sadra Moghadasi <sadra.19@gmail.com>
2024-08-13 13:03:12 +0330

    Add free call in driver call

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -861,85 +861,90 @@ class DriverController extends Controller
             //     return ['result' => true];
             // }
 
+            if (FleetLoad::where('load_id', $load_id)->where('fleet_id', 82)->exists()) {
+                if ($driver->activeDate > date("Y-m-d H:i:s", time()) || $driver->freeCalls > 0) {
 
-            if ($driver->activeDate > date("Y-m-d H:i:s", time()) || $driver->freeCalls > 0) {
+                    if (DriverCall::where('load_id', $load_id)->where('driver_id', $driver->id)->count() > 0) {
+                        return ['result' => true];
+                    }
 
-                if (DriverCall::where('load_id', $load_id)->where('driver_id', $driver->id)->count() > 0) {
-                    return ['result' => true];
-                }
+                    if ($driver->activeDate < date("Y-m-d H:i:s", time())) {
+                        $driver->freeCalls--;
+                        $driver->save();
+                    }
 
-                if ($driver->activeDate < date("Y-m-d H:i:s", time())) {
-                    $driver->freeCalls--;
-                    $driver->save();
-                }
+                    $persian_date = gregorianDateToPersian(date('Y/m/d', time()), '/');
 
-                $persian_date = gregorianDateToPersian(date('Y/m/d', time()), '/');
-
-                // گزارش رانندگان بر اساس تماس
-                $driverCallCount = DriverCallCount::where('driver_id', $driver->id)
-                    ->where('persian_date', $persian_date)
-                    ->first();
-                if (isset($driverCallCount->id)) {
-                    $driverCallCount->calls += 1;
-                    $driverCallCount->created_date = $driver->created_at;
-                    $driverCallCount->save();
-                } else {
-                    $driverCallCount = new DriverCallCount();
-                    $driverCallCount->persian_date = $persian_date;
-                    $driverCallCount->calls = 1;
-                    $driverCallCount->driver_id = $driver->id;
-                    $driverCallCount->created_date = $driver->created_at;
-                    $driverCallCount->save();
-                }
-
-                // فعالیت رانندگان بر اساس تماس
-                if (DriverCall::where('created_at', '>', date("Y-m-d", time()) . " 00:00:00")->where('driver_id', $driver->id)->count() == 0) {
-
-                    // گزارش رانندگان بر اساس ناوگان
-                    $driverCallReport = DriverCallReport::where('fleet_id', $driver->fleet_id)
+                    // گزارش رانندگان بر اساس تماس
+                    $driverCallCount = DriverCallCount::where('driver_id', $driver->id)
                         ->where('persian_date', $persian_date)
                         ->first();
-                    if (isset($driverCallReport->id)) {
-                        $driverCallReport->calls += 1;
-                        $driverCallReport->save();
+                    if (isset($driverCallCount->id)) {
+                        $driverCallCount->calls += 1;
+                        $driverCallCount->created_date = $driver->created_at;
+                        $driverCallCount->save();
                     } else {
-                        $driverCallReport = new DriverCallReport();
-                        $driverCallReport->persian_date = $persian_date;
-                        $driverCallReport->calls = 1;
-                        $driverCallReport->fleet_id = $driver->fleet_id;
-                        $driverCallReport->save();
+                        $driverCallCount = new DriverCallCount();
+                        $driverCallCount->persian_date = $persian_date;
+                        $driverCallCount->calls = 1;
+                        $driverCallCount->driver_id = $driver->id;
+                        $driverCallCount->created_date = $driver->created_at;
+                        $driverCallCount->save();
                     }
-                }
 
-                $driverCall = new DriverCall();
-                $driverCall->driver_id = $driver->id;
-                $driverCall->load_id = $load_id;
-                $driverCall->phoneNumber = $phoneNumber;
-                $driverCall->callingDate = date("Y-m-d");
-                $driverCall->date = gregorianDateToPersian(date('Y/m/d', time()), '/');
-                $driverCall->dateTime = now()->format('H:i:s');
-                $driverCall->latitude = $latitude == 0 ? 0 : $latitude;
-                $driverCall->longitude = $longitude == 0 ? 0 : $longitude;
-                $driverCall->save();
+                    // فعالیت رانندگان بر اساس تماس
+                    if (DriverCall::where('created_at', '>', date("Y-m-d", time()) . " 00:00:00")->where('driver_id', $driver->id)->count() == 0) {
 
-
-                $load = Load::find($load_id);
-
-                if (isset($load->id) && $load->operator_id > 0 || $load->isBot == 1) {
-
-                    $load->driverCallCounter--;
-                    $load->save();
-                    $fleets = json_decode($load->fleets, true);
-                    foreach ($fleets as $fleet) {
-                        if ($fleet['fleet_id'] == 86 && $load->driverCallCounter <= 0) {
-                            $load->delete();
-                        }
-                        if ($fleet['fleet_id'] == 82 && $load->driverCallCounter <= 0) {
-                            $load->delete();
+                        // گزارش رانندگان بر اساس ناوگان
+                        $driverCallReport = DriverCallReport::where('fleet_id', $driver->fleet_id)
+                            ->where('persian_date', $persian_date)
+                            ->first();
+                        if (isset($driverCallReport->id)) {
+                            $driverCallReport->calls += 1;
+                            $driverCallReport->save();
+                        } else {
+                            $driverCallReport = new DriverCallReport();
+                            $driverCallReport->persian_date = $persian_date;
+                            $driverCallReport->calls = 1;
+                            $driverCallReport->fleet_id = $driver->fleet_id;
+                            $driverCallReport->save();
                         }
                     }
-                }
 
+                    $driverCall = new DriverCall();
+                    $driverCall->driver_id = $driver->id;
+                    $driverCall->load_id = $load_id;
+                    $driverCall->phoneNumber = $phoneNumber;
+                    $driverCall->callingDate = date("Y-m-d");
+                    $driverCall->date = gregorianDateToPersian(date('Y/m/d', time()), '/');
+                    $driverCall->dateTime = now()->format('H:i:s');
+                    $driverCall->latitude = $latitude == 0 ? 0 : $latitude;
+                    $driverCall->longitude = $longitude == 0 ? 0 : $longitude;
+                    $driverCall->save();
+
+
+                    $load = Load::find($load_id);
+
+                    if (isset($load->id) && $load->operator_id > 0 || $load->isBot == 1) {
+
+                        $load->driverCallCounter--;
+                        $load->save();
+                        $fleets = json_decode($load->fleets, true);
+                        foreach ($fleets as $fleet) {
+                            if ($fleet['fleet_id'] == 86 && $load->driverCallCounter <= 0) {
+                                $load->delete();
+                            }
+                            if ($fleet['fleet_id'] == 82 && $load->driverCallCounter <= 0) {
+                                $load->delete();
+                            }
+                        }
+                    }
+
+                    return ['result' => true];
+                }
+            } elseif (FleetLoad::where('load_id', $load_id)->where('fleet_id', '!=', 82)->whereHas('cargo', function ($q) {
+                $q->where('userType', 'owner');
+            })->exists()) {
                 return ['result' => true];
             }
         } catch (Exception $exception) {
