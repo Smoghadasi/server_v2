d1a0e8168920d0abf0c78f84c759f66cd226a15a
Sadra Moghadasi <sadra.19@gmail.com>
2024-09-11 10:08:19 +0330

    fix bug

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -91,101 +91,11 @@ class ComplaintController extends Controller
 
     public function storeComplaintDriverAdminMessage(Request $request, ComplaintDriver $complaintDriver)
     {
-
         $complaintDriver->adminMessage = $request->adminMessage;
         $complaintDriver->save();
-
-        try {
-            $driverFCM_tokens = Driver::where('id', $complaintDriver->driver_id)
-                ->whereNotNull('FCM_token')
-                ->pluck('FCM_token');
-            $title = 'ایران ترابر';
-            $body = 'پاسخ تیکت ' . ' ( ' . $complaintDriver->trackingCode . ' ) ' . 'برای شما ارسال شد.';
-            foreach ($driverFCM_tokens as $driverFCM_token) {
-                $this->sendNotification($driverFCM_token, $title, $body, API_ACCESS_KEY_OWNER);
-            }
-        } catch (\Exception $exception) {
-            Log::emergency("----------------------send notification complaintDriver-----------------------");
-            Log::emergency($exception);
-            Log::emergency("---------------------------------------------------------");
-        }
-
-
         return back()->with('success', 'پاسخ مورد نظر ثبت شد');
     }
 
-    private function sendNotification($FCM_token, $title, $body)
-    {
-
-        $serviceAccountPath = asset('assets/zarin-tarabar-firebase-adminsdk-9x6c3-7dbc939cac.json');
-        $serviceAccount = json_decode(file_get_contents($serviceAccountPath), true);
-
-        $clientEmail = $serviceAccount['client_email'];
-        $privateKey = $serviceAccount['private_key'];
-
-        $header = json_encode(['alg' => 'RS256', 'typ' => 'JWT']);
-        $now = time();
-        $expiration = $now + 3600;
-        $payload = json_encode([
-            'iss' => $clientEmail,
-            'scope' => 'https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/firebase.messaging',
-            'aud' => 'https://oauth2.googleapis.com/token',
-            'exp' => $expiration,
-            'iat' => $now
-        ]);
-
-        // Encode to base64
-        $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
-        $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
-        // Create the signature
-        $signatureInput = $base64UrlHeader . "." . $base64UrlPayload;
-        openssl_sign($signatureInput, $signature, $privateKey, 'sha256');
-        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
-        // Create the JWT
-        $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
-        // Exchange JWT for an access token
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://oauth2.googleapis.com/token');
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
-            'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-            'assertion' => $jwt
-        ]));
-        $response = curl_exec($ch);
-        curl_close($ch);
-        $responseData = json_decode($response, true);
-        $accessToken = $responseData['access_token'];
-
-        $url = 'https://fcm.googleapis.com/v1/projects/zarin-tarabar/messages:send';
-        $notification = [
-            "message" => [
-                "token" => $FCM_token,
-                "notification" => [
-                    "title" => $title,
-                    "body" => $body
-                ]
-                // "webpush" => [
-                //     "fcm_options" => [
-                //         "link" => "https://cargo.iran-tarabar.com/780849"
-                //     ]
-                // ]
-            ],
-        ];
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Authorization: Bearer ' . $accessToken,
-            'Content-Type: application/json'
-        ]);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($notification));
-        curl_exec($ch);
-        curl_close($ch);
-    }
 
     ///////////////////////////////////////////////////////////////////////////////////////////
 
