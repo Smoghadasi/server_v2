25474690dfca3373ac451d01c5e76cd1027a654c
Sadra Moghadasi <sadra.19@gmail.com>
2024-09-18 14:05:10 +0330

    Update: search in users By City

--- resources/views/admin/driver/city.blade.php
+++ resources/views/admin/driver/city.blade.php
@@ -25,6 +25,13 @@
                                         @endforeach
                                     </select>
                                 </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label>شماره تلفن :</label>
+                                        <input type="text" name="mobileNumber" class="form-control">
+                                    </div>
+                                </div>
+                                <input type="hidden" name="province_id" value="{{ $provinceCity->id }}">
                             </div>
                         </div>
                         <div class="form-group my-4">
--- resources/views/admin/driver/province.blade.php
+++ resources/views/admin/driver/province.blade.php
@@ -26,6 +26,13 @@
                                     </select>
                                 </div>
                             </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label>شماره تلفن :</label>
+                                    <input type="text" name="mobileNumber" class="form-control">
+                                </div>
+                            </div>
+                            <input type="hidden" name="province_id" value="{{ $usersByProvince->id }}">
                         </div>
                         <div class="form-group my-4">
                             <button class="btn btn-info" type="submit">جستجو</button>
