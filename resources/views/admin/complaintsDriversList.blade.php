bdbeefcabb76d284f2640621bd0ff86ccc22da51
Sadra Moghadasi <sadra.19@gmail.com>
2024-10-17 09:22:20 +0330

    Add shamsi created date and updated date

--- resources/views/admin/complaintsDriversList.blade.php
+++ resources/views/admin/complaintsDriversList.blade.php
@@ -7,88 +7,90 @@
             شکایات و انتقادات رانندگان
         </h5>
         <div class="card-body">
-
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>راننده</th>
-                    <th>عنوان</th>
-                    {{-- <th>شکایت یا انتقاد از</th> --}}
-                    {{-- <th>شماره تلفن مورد نظر</th> --}}
-                    <th>کد پیگیری</th>
-                    <th>متن پیام</th>
-                    <th>پاسخ ادمین</th>
-                    <th>تاریخ ثبت</th>
-                    <th>عملیات</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($complaintsDrivers as $key => $complaintDriver)
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
                     <tr>
-                        <td>{{ (($complaintsDrivers->currentPage()-1) * $complaintsDrivers->perPage()) + ($key + 1) }}</td>
-                        <td><a href="{{ route('driver.detail', $complaintDriver->driverApp->id) }}">{{ $complaintDriver->driver }}</a></td>
-                        <td>{{ $complaintDriver->title }}</td>
-                        {{-- <td>{{ $complaintDriver->complaint }}</td> --}}
-                        {{-- <td>{{ $complaintDriver->phoneNumber }}</td> --}}
-                        <td>{{ $complaintDriver->trackingCode }}</td>
-                        <td>{{ $complaintDriver->message }}</td>
-                        <td>{{ $complaintDriver->adminMessage }}</td>
-                        @php
-                            $pieces = explode(' ', $complaintDriver->created_at);
-                        @endphp
-                        <td>
-                            {{ gregorianDateToPersian($complaintDriver->created_at, '-', true) . ' ( ' . $pieces[1] . ' ) ' }}
-                        </td>
-
-                        <td>
-                            <button type="button" class="btn btn-primary mb-3 btn-sm text-nowrap"
-
-                                    data-bs-toggle="modal"
-                                    data-bs-target="#adminMessageForm_{{ $complaintDriver->id }}">
-                                پاسخ به راننده
-                            </button>
-
-                            <div id="adminMessageForm_{{ $complaintDriver->id }}" class="modal fade" role="dialog">
-                                <div class="modal-dialog">
-
-                                    <!-- Modal content-->
-                                    <form
-                                        action="{{ url('admin/storeComplaintDriverAdminMessage') }}/{{ $complaintDriver->id }}"
-                                        method="post"
-                                        class="modal-content">
-                                        @csrf
-                                        <div class="modal-header">
-                                            <h4 class="modal-title">پاسخ به راننده</h4>
-                                        </div>
-                                        <div class="modal-body text-right">
-
-                                            <div>
-                                                عنوان پیام :
-                                                {{ $complaintDriver->title }}
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label>متن پاسخ ادمین :</label>
-                                                <textarea class="form-control" name="adminMessage" id="adminMessage"
-                                                          placeholder="پاسخ ادمین"></textarea>
-                                            </div>
-                                        </div>
-                                        <div class="modal-footer text-left">
-                                            <button type="submit" class="btn btn-primary mr-1">ثبت پاسخ</button>
-                                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
-                                                انصراف
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </td>
+                        <th>#</th>
+                        <th>راننده</th>
+                        <th>عنوان</th>
+                        {{-- <th>شکایت یا انتقاد از</th> --}}
+                        {{-- <th>شماره تلفن مورد نظر</th> --}}
+                        <th>کد پیگیری</th>
+                        <th>متن پیام</th>
+                        <th>پاسخ ادمین</th>
+                        <th>تاریخ ثبت</th>
+                        <th>عملیات</th>
                     </tr>
-                @endforeach
-                </tbody>
+                    </thead>
+                    <tbody>
+                    @foreach($complaintsDrivers as $key => $complaintDriver)
+                        <tr>
+                            <td>{{ (($complaintsDrivers->currentPage()-1) * $complaintsDrivers->perPage()) + ($key + 1) }}</td>
+                            <td><a href="{{ route('driver.detail', $complaintDriver->driverApp->id) }}">{{ $complaintDriver->driver }}</a></td>
+                            <td>{{ $complaintDriver->title }}</td>
+                            {{-- <td>{{ $complaintDriver->complaint }}</td> --}}
+                            {{-- <td>{{ $complaintDriver->phoneNumber }}</td> --}}
+                            <td>{{ $complaintDriver->trackingCode }}</td>
+                            <td>{{ $complaintDriver->message }}</td>
+                            <td>{{ $complaintDriver->adminMessage }}</td>
+                            @php
+                                $pieces = explode(' ', $complaintDriver->created_at);
+                            @endphp
+                            <td>
+                                {{ gregorianDateToPersian($complaintDriver->created_at, '-', true) . ' ( ' . $pieces[1] . ' ) ' }}
+                            </td>
 
-            </table>
+                            <td>
+                                <button type="button" class="btn btn-primary mb-3 btn-sm text-nowrap"
+
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#adminMessageForm_{{ $complaintDriver->id }}">
+                                    پاسخ به راننده
+                                </button>
+
+                                <div id="adminMessageForm_{{ $complaintDriver->id }}" class="modal fade" role="dialog">
+                                    <div class="modal-dialog">
+
+                                        <!-- Modal content-->
+                                        <form
+                                            action="{{ url('admin/storeComplaintDriverAdminMessage') }}/{{ $complaintDriver->id }}"
+                                            method="post"
+                                            class="modal-content">
+                                            @csrf
+                                            <div class="modal-header">
+                                                <h4 class="modal-title">پاسخ به راننده</h4>
+                                            </div>
+                                            <div class="modal-body text-right">
+
+                                                <div>
+                                                    عنوان پیام :
+                                                    {{ $complaintDriver->title }}
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label>متن پاسخ ادمین :</label>
+                                                    <textarea class="form-control" name="adminMessage" id="adminMessage"
+                                                              placeholder="پاسخ ادمین"></textarea>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer text-left">
+                                                <button type="submit" class="btn btn-primary mr-1">ثبت پاسخ</button>
+                                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
+                                                    انصراف
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+
+                </table>
+            </div>
+
 
             <div class="mt-3">
                 {{ $complaintsDrivers }}
