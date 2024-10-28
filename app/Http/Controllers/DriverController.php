80c5749054668b68f85615cfd35012cfd8b1ddba
Sadra Moghadasi <sadra.19@gmail.com>
2024-10-28 09:44:36 +0330

    add free call

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -851,25 +851,20 @@ class DriverController extends Controller
         try {
             $load = Load::where('id', '=', $load_id)->first();
 
-            // $owner = Owner::where('mobileNumber', $load->mobileNumberForCoordination)->whereNotNull('FCM_token')->first();
-            // $cityFrom = ProvinceCity::findOrFail($load->origin_city_id);
-            // $cityTo = ProvinceCity::findOrFail($load->destination_city_id);
-
-            // if ($owner) {
-            //     try {
-            //         $title = 'ایران ترابر صاحبان بار';
-            //         $body = $driver->name . ' ' . $driver->lastName . ' راننده ' . '(' . $driver->fleetTitle . ')' . ' جهت حمل بار از ' . $cityFrom->name . ' به ' . $cityTo->name . ' با شما تماس گرفته است.';
-
-            //         $this->sendNotification($owner->FCM_token, $title, $body);
-            //     } catch (\Exception $exception) {
-            //         Log::emergency("----------------------send notif storeInquiryToLoad-----------------------");
-            //         Log::emergency($exception);
-            //         Log::emergency("---------------------------------------------------------");
-            //     }
-            // }
-
             if ($load === null) {
                 return ['result' => 2];
+            }
+
+            try {
+                $fleetLoad = FleetLoad::where('fleet_id', $driver->fleet_id)->where('load_id', $load_id)->first();
+                if ($fleetLoad) {
+                    $fleetIds = [43, 48, 49, 50, 51, 52, 53, 55, 56, 57, 58, 59, 60, 61, 66, 74, 78, 79];
+                    if (in_array($driver->fleet_id, $fleetIds)) {
+                        return $this->checkCreditDriver($driver, $load_id, $phoneNumber, false);
+                    }
+                }
+            } catch (\Exception $e) {
+                Log::warning($e->getMessage());
             }
 
             if ($driver->activeDate > date("Y-m-d H:i:s", time()) || $driver->freeCalls > 0) {
@@ -1032,6 +1027,85 @@ class DriverController extends Controller
         return [
             'result' => false
         ];
+    }
+
+    public function checkCreditDriver($driver, $load_id, $phoneNumber, $checkFreeCall)
+    {
+        if (DriverCall::where('load_id', $load_id)->where('driver_id', $driver->id)->count() > 0) {
+            return ['result' => true];
+        }
+        if ($checkFreeCall == true) {
+            if ($driver->activeDate < date("Y-m-d H:i:s", time())) {
+                $driver->freeCalls--;
+                $driver->save();
+            }
+        }
+
+        $persian_date = gregorianDateToPersian(date('Y/m/d', time()), '/');
+
+        // گزارش رانندگان بر اساس تماس
+        $driverCallCount = DriverCallCount::where('driver_id', $driver->id)
+            ->where('persian_date', $persian_date)
+            ->first();
+        if (isset($driverCallCount->id)) {
+            $driverCallCount->calls += 1;
+            $driverCallCount->created_date = $driver->created_at;
+            $driverCallCount->save();
+        } else {
+
+            $driverCallCount = new DriverCallCount();
+            $driverCallCount->persian_date = $persian_date;
+            $driverCallCount->calls = 1;
+            $driverCallCount->driver_id = $driver->id;
+            $driverCallCount->created_date = $driver->created_at;
+            $driverCallCount->save();
+        }
+
+        // فعالیت رانندگان بر اساس تماس
+        if (DriverCall::where('created_at', '>', date("Y-m-d", time()) . " 00:00:00")->where('driver_id', $driver->id)->count() == 0) {
+            // گزارش رانندگان بر اساس ناوگان
+            $driverCallReport = DriverCallReport::where('fleet_id', $driver->fleet_id)
+                ->where('persian_date', $persian_date)
+                ->first();
+            if (isset($driverCallReport->id)) {
+                $driverCallReport->calls += 1;
+                $driverCallReport->save();
+            } else {
+                $driverCallReport = new DriverCallReport();
+                $driverCallReport->persian_date = $persian_date;
+                $driverCallReport->calls = 1;
+                $driverCallReport->fleet_id = $driver->fleet_id;
+                $driverCallReport->save();
+            }
+        }
+
+        $driverCall = new DriverCall();
+        $driverCall->driver_id = $driver->id;
+        $driverCall->load_id = $load_id;
+        $driverCall->phoneNumber = $phoneNumber;
+        $driverCall->callingDate = date("Y-m-d");
+        $driverCall->date = gregorianDateToPersian(date('Y/m/d', time()), '/');
+        $driverCall->dateTime = now()->format('H:i:s');
+        $driverCall->latitude = 0;
+        $driverCall->longitude = 0;
+        $driverCall->save();
+
+
+        $load = Load::find($load_id);
+
+        if (isset($load->id) && $load->operator_id > 0 || $load->isBot == 1) {
+
+            $load->driverCallCounter--;
+            $load->save();
+            $fleets = json_decode($load->fleets, true);
+            foreach ($fleets as $fleet) {
+                if (($fleet['fleet_id'] == 86 || $fleet['fleet_id'] == 82) && $load->driverCallCounter <= 0) {
+                    $load->delete();
+                }
+            }
+        }
+
+        return ['result' => true];
     }
 
     public function getPackagesInfo()
