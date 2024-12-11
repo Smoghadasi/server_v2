c481431df454f0d006d4fabe76abcbe10e4c2f2c
Sadra Moghadasi <sadra.19@gmail.com>
2024-12-11 10:43:33 +0330

    update waiting customer payment

--- resources/views/admin/reporting/driversPaymentReport.blade.php
+++ resources/views/admin/reporting/driversPaymentReport.blade.php
@@ -13,13 +13,13 @@
                     @csrf
                     <div class="col-lg-3">
                         <input type="tel" class="form-control" name="mobileNumber" placeholder="شماره تلفن"
-                        @if(isset($driver->mobileNumber)) value="{{ $driver->mobileNumber }}" @endif>
+                            @if (isset($driver->mobileNumber)) value="{{ $driver->mobileNumber }}" @endif>
                     </div>
                     <button class="btn btn-primary col-lg-2">جستجو</button>
                 </form>
             </div>
 
-            @if(isset($driver->id))
+            @if (isset($driver->id))
 
                 <div class="col-lg-12 mt-3 pt-3 border-top">
                     نام و نام خانوادگی راننده :
@@ -31,42 +31,49 @@
 
                 <table class="table mt-2">
                     <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>مبلغ</th>
-                        <th>وضعیت</th>
-                        <th>نوع</th>
-                        <th>تاریخ</th>
-                    </tr>
+                        <tr>
+                            <th>#</th>
+                            <th>مبلغ</th>
+                            <th>وضعیت</th>
+                            <th>نوع</th>
+                            <th>بانک</th>
+                            <th>تاریخ</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    @forelse($transactions as $key => $transaction)
-                        <tr>
-                            <td>{{ $key + 1 }}</td>
-                            <td>{{ $transaction->amount }}</td>
-                            <td>
-                                @if($transaction->status > 0)
-                                    <span class="alert alert-success m-1 p-1">
-                                        پرداخت شده
-                                    </span>
-                                @else
-                                    <span class="alert alert-danger m-1 p-1">
-                                        پرداخت نشده
-                                    </span>
-                                @endif
-                            </td>
-                            <td>{{ $transaction->payment_type == 'online' ? 'آنلاین' : 'کارت به کارت' }}</td>
-                            <td>{{ gregorianDateToPersian(str_replace('-','/',$transaction->created_at), '/',true) }}</td>
-                        </tr>
-                    @empty
-                    <tr class="text-center">
-                        <td colspan="10">فیلد مورد نظر خالی می باشد</td>
-                    </tr>
-                    @endforelse
+                        @forelse($transactions as $key => $transaction)
+                            <tr>
+                                <td>{{ $key + 1 }}</td>
+                                <td>{{ $transaction->amount }}</td>
+                                <td>
+                                    @if ($transaction->status > 2)
+                                        <span class="alert alert-success m-1 p-1">
+                                            پرداخت شده
+                                        </span>
+                                    @elseif ($transaction->status == 2)
+                                        <span class="alert alert-warning m-1 p-1">
+                                            در انتظار پرداخت
+                                        </span>
+                                    @else
+                                        <span class="alert alert-danger m-1 p-1">
+                                            پرداخت نشده
+                                        </span>
+                                    @endif
+                                </td>
+                                <td>{{ $transaction->bank_name }}</td>
+                                <td>{{ $transaction->payment_type == 'online' ? 'آنلاین' : 'کارت به کارت' }}</td>
+                                <td>{{ gregorianDateToPersian(str_replace('-', '/', $transaction->created_at), '/', true) }}
+                                </td>
+                            </tr>
+                        @empty
+                            <tr class="text-center">
+                                <td colspan="10">فیلد مورد نظر خالی می باشد</td>
+                            </tr>
+                        @endforelse
                     </tbody>
                 </table>
 
-                @if($transactions!=[])
+                @if ($transactions != [])
                     {{ $transactions }}
                 @endif
             @endif
@@ -74,6 +81,3 @@
     </div>
 
 @stop
-
-
-
