5c9aa94e6e83c978cc41f5474bec6d8fa233550b
Sadra Moghadasi <sadra.19@gmail.com>
2024-12-11 12:43:12 +0330

    update sina bnak in payment

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -787,7 +787,7 @@ class PayController extends Controller
         $params = array(
             "LoginAccount" => PIN_SINA,
             "Amount" => $amount,
-            "OrderId" => date('YmdHisu') . $driver->id,
+            "OrderId" => date('YmdHis') . $driver->id,
             "CallBackUrl" => $callbackUrl,
             "AdditionalData" => '',
             "Originator" => ''
