594af6f2be36f49facd45af4d6116bb3a6460773
Sadra Moghadasi <sadra.19@gmail.com>
2024-12-24 08:53:34 +0330

    fix bug contact

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -123,6 +123,9 @@ class ContactUsController extends Controller
         $contact->result = strlen($request->result) ? $request->result : "نتیجه ای ثبت نشده!";
         $contact->save();
 
+        $contactUs->status = true;
+        $contactUs->save();
+
         if ($request->notification == 'on') {
             try {
                 if ($contactUs->role == 'driver') {
