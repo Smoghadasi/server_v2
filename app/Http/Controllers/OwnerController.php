5726059582902947277d262f6f8cfd5595ac1485
sadra <sadra.19@gmail.com>
2025-01-11 11:40:18 +0330

    add owners in support

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -30,11 +30,7 @@ class OwnerController extends Controller
         $ownerAcceptCounts = Owner::where('isAuth', 1)->count();
         $ownerRejectedCounts = Owner::where('isRejected', 1)->count();
 
-        if (auth()->user()->role == 'admin') {
-            $owners = Owner::orderByDesc('created_at')->paginate(10);
-        } elseif (auth()->user()->role == 'operator') {
-            $owners = collect();
-        }
+        $owners = collect();
 
 
         $fleets = Fleet::all();
