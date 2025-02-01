90e860a657de262f0f891c72e800fbdea4095262
Sadra Moghadasi <sadra.19@gmail.com>
2025-02-01 15:16:39 +0330

    add discrepancies

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -101,26 +101,6 @@
 
             </ul>
         </li>
-        @if (in_array('discrepancies', auth()->user()->userAccess))
-
-            <li class="menu-item">
-                <a href="javascript:void(0);" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons bx bx-credit-card"></i>
-                    <div data-i18n="pais">حسابداری
-
-                    </div>
-                </a>
-                <ul class="menu-sub">
-                    @if (in_array('discrepancies', auth()->user()->userAccess))
-                        <li class="menu-item">
-                            <a class="menu-link" href="{{ route('discrepancy.index') }}">
-                                <div data-i18n="Without menu">صورت مغایرت</div>
-                            </a>
-                        </li>
-                    @endif
-                </ul>
-            </li>
-        @endif
 
 
         <li class="menu-item">
@@ -684,24 +664,26 @@
                 </ul>
             </li>
         @endif
-        {{-- @if (in_array('accounting', auth()->user()->userAccess)) --}}
-        <li class="menu-item">
-            <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bx-credit-card"></i>
-                <div data-i18n="Layouts">حسابداری</div>
-            </a>
-            <ul class="menu-sub">
-                {{-- @if (in_array('accounting', auth()->user()->userAccess)) --}}
-                <li class="menu-item">
-                    <a class="menu-link" href="{{ route('slider.index') }}">
-                        <span>صورت مغایرت</span>
-                    </a>
-                </li>
-                {{-- @endif --}}
+        @if (in_array('discrepancies', auth()->user()->userAccess))
 
-            </ul>
-        </li>
-        {{-- @endif --}}
+            <li class="menu-item">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-credit-card"></i>
+                    <div data-i18n="pais">حسابداری
+
+                    </div>
+                </a>
+                <ul class="menu-sub">
+                    @if (in_array('discrepancies', auth()->user()->userAccess))
+                        <li class="menu-item">
+                            <a class="menu-link" href="{{ route('discrepancy.index') }}">
+                                <div data-i18n="Without menu">صورت مغایرت</div>
+                            </a>
+                        </li>
+                    @endif
+                </ul>
+            </li>
+        @endif
 
     </ul>
 </aside>
