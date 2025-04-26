3fd3593c2d43e15940de1fad227a3b27d9ad7dec
Sadra Moghadasi <sadra.19@gmail.com>
2025-04-26 13:29:21 +0330

    Add: non repeate driver report activity

--- resources/views/admin/reporting/nonRepeate.blade.php
+++ resources/views/admin/reporting/nonRepeate.blade.php
@@ -1,32 +1,38 @@
-<div class="card">
-    <h5 class="card-header">گزارش فعالیت رانندگان غیر تکراری</h5>
-    <div class="card-body">
-        <div class="text-center h6"> گزارش فعالیت راننده ها از ماه قبل</div>
-        <canvas id="activityReportOfDriversFromPreviousMonth" style="width:100%;max-width:100%"></canvas>
+@extends('layouts.dashboard')
+@section('content')
+    <div class="card">
+        <h5 class="card-header">گزارش فعالیت رانندگان غیر تکراری</h5>
+        <div class="card-body">
+            <div class="text-center h6"> گزارش فعالیت راننده ها از ماه قبل</div>
+            <canvas id="activityReportOfDriversFromPreviousMonth" style="width:100%;max-width:100%"></canvas>
+        </div>
     </div>
-</div>
+@endsection
+
 
 @section('script')
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        fetch("{{ route('admin.reporting.nonRepeate.data') }}")
-            .then(response => response.json())
-            .then(data => {
-                new Chart("activityReportOfDriversFromPreviousMonth", {
-                    type: "line",
-                    data: {
-                        labels: data.labels,
-                        datasets: [{
-                            data: data.values,
-                            borderColor: "blue",
-                            fill: false
-                        }]
-                    },
-                    options: {
-                        legend: { display: false }
-                    }
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            fetch("{{ route('admin.reporting.nonRepeate.data') }}")
+                .then(response => response.json())
+                .then(data => {
+                    new Chart("activityReportOfDriversFromPreviousMonth", {
+                        type: "line",
+                        data: {
+                            labels: data.labels,
+                            datasets: [{
+                                data: data.values,
+                                borderColor: "blue",
+                                fill: false
+                            }]
+                        },
+                        options: {
+                            legend: {
+                                display: false
+                            }
+                        }
+                    });
                 });
-            });
-    });
-</script>
+        });
+    </script>
 @endsection
