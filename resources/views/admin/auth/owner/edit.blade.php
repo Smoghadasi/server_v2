b6524e0498cfc0f40525e3459ced06d0fd2877e1
Sadra Moghadasi <sadra.19@gmail.com>
2025-04-29 11:17:01 +0330

    Add is accepeted in owner search operator

--- resources/views/admin/auth/owner/edit.blade.php
+++ resources/views/admin/auth/owner/edit.blade.php
@@ -39,7 +39,7 @@
                                     value="{{ $ownerAuth->mobileNumber }}" id="mobileNumber">
                             </div>
                         </div>
-                        @if (auth()->user()->role == 'admin' || auth()->user()->id == 29)
+                        @if (auth()->user()->role == 'admin' || auth()->user()->id == 29 || auth()->user()->id == 69)
                             <div class="mb-3 row">
                                 <label for="isOwner" class="col-md-2 col-form-label">نوع</label>
                                 <div class="col-md-10">
