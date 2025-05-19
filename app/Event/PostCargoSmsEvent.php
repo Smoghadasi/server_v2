754258853ed2cf72f94a1a3924584f8f466567bc
Sadra Moghadasi <sadra.19@gmail.com>
2025-05-19 13:35:37 +0330

    Add SendNotificationForNearDriver

--- app/Event/PostCargoSmsEvent.php
+++ app/Event/PostCargoSmsEvent.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class PostCargoSmsEvent implements ShouldQueue
+class PostCargoSmsEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
