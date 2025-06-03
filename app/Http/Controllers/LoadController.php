8d894350e620a91d06d590e0b2db130e2720c442
Sadra Moghadasi <sadra.19@gmail.com>
2025-06-03 09:13:12 +0330

    pref: driver visit load

--- app/Http/Controllers/LoadController.php
+++ app/Http/Controllers/LoadController.php
@@ -2475,7 +2475,7 @@ class LoadController extends Controller
 
     public function driverVisitLoads($load_id)
     {
-        $load = Load::findOrFail($load_id);
+        $load = Load::withTrashed()->findOrFail($load_id);
         $driverVisitLoads = DriverVisitLoad::with('driver')->where('load_id', $load_id)->paginate(20);
 
         return view('admin.driver.driverVisitLoad', compact('driverVisitLoads', 'load'));
