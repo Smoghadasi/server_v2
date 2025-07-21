7dba9bfac70b75b667de7ed09355c851b1c452eb
Sadra Moghadasi <sadra.19@gmail.com>
2025-07-21 10:18:39 +0330

    Pref: Store cargo operator

--- resources/views/admin/storeCargoOperators/show.blade.php
+++ resources/views/admin/storeCargoOperators/show.blade.php
@@ -22,10 +22,12 @@
                             <td>
                                 @if ($cargo->operator_id > 0)
                                     @if ($cargo->status == 1 && $cargo->rejected == 0)
-                                        <span class="text-success">ثبت توسط: {{ $cargo->operator->name }} {{ $cargo->operator->lastName }}</span>
+                                        <span class="text-success">ثبت توسط: </span>
+                                        {{ $cargo->operator->name }} {{ $cargo->operator->lastName }}
                                     @endif
                                     @if ($cargo->rejected == 1 && $cargo->status == 1)
-                                        <span class="text-danger">رد توسط : {{ $cargo->operator->name }} {{ $cargo->operator->lastName }} </span>
+                                        <span class="text-danger">رد توسط : </span>
+                                        {{ $cargo->operator->name }} {{ $cargo->operator->lastName }}
                                     @endif
                                     @if ($cargo->status == 0 && $cargo->rejected == 0)
                                         <span class="text-primary">در حال بررسی</span>
