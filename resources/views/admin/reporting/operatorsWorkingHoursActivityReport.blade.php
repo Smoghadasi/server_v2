d6b44577ade1dbb2f5cf2a7951e3ddef21936985
Sadra Moghadasi <sadra.19@gmail.com>
2025-09-28 15:41:14 +0330

    Update: operatorsWorkingHoursActivityReport

--- resources/views/admin/reporting/operatorsWorkingHoursActivityReport.blade.php
+++ resources/views/admin/reporting/operatorsWorkingHoursActivityReport.blade.php
@@ -24,53 +24,67 @@
 
             <table class="table">
                 <thead>
-                <tr>
-                    <th>#</th>
-                    <th>نام اپراتور</th>
-                    <th>ساعت شروع</th>
-                    <th>ساعت پایان</th>
-                    <th>میزان فعالیت به دقیقه / ساعت</th>
-                    <th>تعداد بار های ثبت شده</th>
-                    <th>
-                        وضعیت فعلی ( <span class="text-success">آنلاین</span> / <span class="text-danger">آفلاین</span> )
-                    </th>
-                    <th>گزارش</th>
-                </tr>
+                    <tr>
+                        <th>#</th>
+                        <th>نام اپراتور</th>
+                        <th>ساعت شروع</th>
+                        <th>ساعت پایان</th>
+                        <th>میزان فعالیت به دقیقه / ساعت</th>
+                        <th>تعداد بار های ثبت شده</th>
+                        <th>
+                            وضعیت فعلی ( <span class="text-success">آنلاین</span> / <span class="text-danger">آفلاین</span>
+                            )
+                        </th>
+                        <th>گزارش</th>
+                    </tr>
                 </thead>
                 <tbody>
-                @php
-                    $i=1;
-                @endphp
+                    @php
+                        $i = 1;
+                        $totalLoads = 0; // متغیر برای جمع کل بارها
 
-                @foreach($users as $user)
+                    @endphp
 
+                    @foreach ($users as $user)
+                        <tr>
+                            <td>{{ $i++ }}</td>
+                            <td>{{ $user->name }} {{ $user->lastName }}</td>
+                            <td>{{ $user->firstLoad()?->created_at->format('H:i:s') ?? '-' }}</td>
+
+                            <td>{{ $user->lastLoad()?->created_at->format('H:i:s') ?? '-' }}</td>
+                            <td>{{ $user->userActivityReport * 5 }} دقیقه | {{ ($user->userActivityReport * 5) % 60 }}
+                                : {{ intdiv($user->userActivityReport * 5, 60) }} ساعت
+                            </td>
+                            <td>{{ $user->numOfLoads() }}
+                                @php
+                                    $totalLoads += $user->numOfLoads(); // جمع کردن تعداد بارها
+                                @endphp
+
+                            </td>
+                            <td>
+                                @if (Cache::has('user-is-online-' . $user->user_id))
+                                    <span class="text-success">آنلاین</span>
+                                @else
+                                    <span class="text-danger">آفلاین</span>
+                                @endif
+                            </td>
+                            <td>
+                                <button disabled class="btn btn-primary btn-sm">گزارش گیری</button>
+                            </td>
+                        </tr>
+                    @endforeach
                     <tr>
-                        <td>{{ $i++ }}</td>
-                        <td>{{ $user->name }} {{ $user->lastName }}</td>
-                        <td>{{ $user->firstLoad()?->created_at->format('H:i:s') ?? '-' }}</td>
-
-                        <td>{{ $user->lastLoad()?->created_at->format('H:i:s') ?? '-' }}</td>
-                        <td>{{ $user->userActivityReport * 5 }} دقیقه  | {{ (($user->userActivityReport * 5) % 60) }}
-                            : {{ intdiv($user->userActivityReport * 5, 60) }} ساعت
-                        </td>
-                        <td>{{ $user->numOfLoads() }}</td>
-                        <td>
-                            @if(Cache::has('user-is-online-' . $user->user_id))
-                                <span class="text-success">آنلاین</span>
-                            @else
-                                <span class="text-danger">آفلاین</span>
-                            @endif
-                        </td>
-                        <td>
-                            <button class="btn btn-primary btn-sm">گزارش گیری</button>
-                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>{{ $totalLoads }}</td>
+                        <td></td>
+                        <td></td>
                     </tr>
-                @endforeach
                 </tbody>
             </table>
         </div>
     </div>
 @stop
-
-
-
