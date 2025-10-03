54eb3ef59ca2bc528c687106280ab67e9abe9de9
sadra <sadra.19@gmail.com>
2025-10-03 16:54:42 +0330

    Pref: change cookie name

--- config/session.php
+++ config/session.php
@@ -126,10 +126,7 @@ return [
     |
     */
 
-    'cookie' => env(
-        'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'site'), '_').'_session'
-    ),
+    'cookie' => 'myapp_session',
 
     /*
     |--------------------------------------------------------------------------
