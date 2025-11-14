63067ba1668995f5c90a35b77b6ad28209472d0a
sadra <sadra.19@gmail.com>
2025-11-14 14:21:04 +0330

    Add: driver list

--- resources/views/admin/driver/reportByFleetType.blade.php
+++ resources/views/admin/driver/reportByFleetType.blade.php
@@ -73,7 +73,7 @@
                                 <td>{{ $driver->mobileNumber }}</td>
                                 <td>
                                     <a class="btn btn-primary"
-                                        href="{{ url('admin/driverInfo') }}/{{ $driver->driver_id }}">جزئیات</a>
+                                        href="{{ url('admin/driverInfo') }}/{{ $driver->driver_id ?? $driver->id }}">جزئیات</a>
                                 </td>
                             </tr>
                         @endforeach
