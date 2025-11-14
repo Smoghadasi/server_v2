e92ea5e12ea2480daf8924a08cd22cdc3304faa6
sadra <sadra.19@gmail.com>
2025-11-14 19:06:25 +0330

    fix sort

--- app/Http/Controllers/ReportingController.php
+++ app/Http/Controllers/ReportingController.php
@@ -99,21 +99,26 @@ class ReportingController extends Controller
         }
 
         if ($type == 'notActiveSubscription') {
+            // رانندگانی که تراکنش مثبت داشته‌اند (مطابق آمار active/notActive)
+
 
             return $drivers = DB::table('drivers')
                 ->join('driver_activities', 'driver_activities.driver_id', '=', 'drivers.id')
                 ->where('driver_activities.created_at', '>', $date)
                 ->where('drivers.fleet_id', $fleetId)
-                ->whereIn('drivers.id', $driverIds)
+
                 ->whereNotIn('drivers.id', $driverTIds)
+
                 ->where(function ($q) use ($now) {
                     $q->whereNull('drivers.activeDate')
                         ->orWhere('drivers.activeDate', '<', $now);
                 })
 
+                ->select(
+                    'drivers.*',
+                )
                 ->distinct('drivers.id')
                 ->count('drivers.id');
-            return view('admin.driver.reportByFleetType', compact('drivers'));
         }
     }
 
