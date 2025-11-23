7748f9f7c7d043d9e534eb3c32f4faf51c68cfd3
Sadra Moghadasi <sadra.19@gmail.com>
2025-11-23 14:31:55 +0330

    Update

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -18,6 +18,7 @@ class ChannelController extends Controller
         $botNumbers = Channel::select('bot_number')->distinct()
             ->where('bot_number', '!=', 'null')
             ->pluck('bot_number');
+        return $botNumbers;
 
         // فیلتر بر اساس تب انتخاب شده
         $query = Channel::query();
